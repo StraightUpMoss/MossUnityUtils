a3894ab04e230f9c765461c7fd0a8f7fee8d7742
StraightUpMoss <liamanson1111@gmail.com>
2025-10-24 19:23:49 -0500

    Fixed an issue with singleton

--- Runtime/GenericUtil/Singleton.cs
+++ Runtime/GenericUtil/Singleton.cs
@@ -4,14 +4,14 @@ namespace MossUnityUtils.GenericUtil
 
     public class Singleton<T> : MonoBehaviour
     {
-        public static Singleton<T> Instance { get; private set; }
+        public static T Instance { get; private set; }
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         protected virtual void Awake()
         {
             if (Instance == null)
             {
-                Instance = this;
+                Instance = GetComponent<T>();
             }
             else
             {
